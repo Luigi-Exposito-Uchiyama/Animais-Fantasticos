3ca39212b2da595aa8e0bcc7c9733fb165002294
Luigi Uchiyama <luigi_uchiyama@outlook.com>
2024-02-16 09:04:41 -0300

    projeto arrumado

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@ import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
 import MenuMobile from './modules/menu-mobile.js';
 import Functionamento from './modules/funcionamento.js';
+import fetchAnimais from './modules/fetch-animais.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
 import ScrollAnima from './modules/scroll-anima.js';
 import SlideNav from './modules/slide.js';
-import fetchAnimais from './modules/fetch-animais.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -38,6 +39,8 @@ const funcionamento = new Functionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
 fetchAnimais('./animaisapi.json', '.numeros-grid');
+
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
